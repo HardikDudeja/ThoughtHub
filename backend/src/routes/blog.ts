e62ebd9bed0ce56399cb308dff3bacde61cc9bfe
HardikDudeja <hardik.dudeja39@gmail.com>
2024-08-28 20:04:20 +0530

    api integration tesing done

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -15,8 +15,7 @@ export const blogRouter = new Hono<{
 }>();
 
 blogRouter.use(async (c, next) => {
-    console.log("in auth middleware");
-    const authHeader = c.req.header('Authorization');
+    const authHeader = c.req.header('Authorization')?.split(' ')[1];
     if(!authHeader){
         return c.json({message: 'Not Authorised'}, 401)
     }
@@ -71,24 +70,6 @@ blogRouter.put('/', async (c) => {
     }
 });
 
-// get particular blog
-blogRouter.get('/', async (c) => {
-    const prisma = new PrismaClient({
-        datasourceUrl: c.env.DATABASE_URL,}).$extends(withAccelerate());
-    const body = await c.req.json();
-    try {
-        const blog = await prisma.blog.findUnique({
-            where: { id: body.id }
-        });
-        if (!blog) {
-            return c.json({ message: 'Blog not found' }, 404);
-        }
-        return c.json({blog});
-    } catch (error) {
-        throw new HTTPException(500, { message: 'Error retrieving blog', cause: error });
-    }
-});
-
 // bulk blog
 // TODO: add pagination
 blogRouter.get('/bulk', async (c) => {
@@ -103,3 +84,22 @@ blogRouter.get('/bulk', async (c) => {
         throw new HTTPException(500, { message: 'Error retrieving blogs', cause: error });
     }
 });
+
+// get particular blog
+blogRouter.get('/:id', async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,}).$extends(withAccelerate());
+    const id = c.req.param('id');
+    try {
+        const blog = await prisma.blog.findUnique({
+            where: { id: Number(id) }
+        });
+        if (!blog) {
+            return c.json({ message: 'Blog not found' }, 404);
+        }
+        return c.json({blog});
+    } catch (error) {
+        throw new HTTPException(500, { message: 'Error retrieving blog', cause: error });
+    }
+});
+
